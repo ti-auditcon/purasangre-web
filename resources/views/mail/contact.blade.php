d8968f336f97137962755d2e6fa811502c87532e
Juan <jberrioscifuentes@gmail.com>
2019-03-22 11:37:23 -0300

    =

--- resources/views/mail/contact.blade.php
+++ resources/views/mail/contact.blade.php
@@ -1,16 +1,14 @@
-<p>Mensaje enviado desde la web</P>
+<p>Mensaje enviado desde el formulario de Contacto</P>
 
 <h4>
   Datos Generales:
 </h4>
-
 <p>Enviado por: {{ $name }}</p>
 <p>Email: {{ $email }}</p>
 
 <h4>
   Mensaje:
 </h4>
-
 <p>
   {{ $mensaje }}
 </p>
