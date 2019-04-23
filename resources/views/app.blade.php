5f607e6f5becc046d0272b0165299bbef6a6c771
Juan <jberrioscifuentes@gmail.com>
2019-04-23 18:30:46 -0400

    prueba

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -41,6 +41,7 @@
     <!-- [if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js" type="text/javascript"></script><![endif] -->
     <script type="text/javascript">!function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);</script>
 
+    <!-- Update de Tag Manageeeer -->
 
     <!-- Google Tag Manager -->
     <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
