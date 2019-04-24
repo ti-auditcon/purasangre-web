a84cb0b9ad9b744d81ad31775c9b79da7c6289ea
Juan <jberrioscifuentes@gmail.com>
2019-04-24 11:54:06 -0400

    Facebook customer chat

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,6 +49,32 @@
     height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     <!-- End Google Tag Manager (noscript) -->
 
+    <!-- Load Facebook SDK for JavaScript -->
+    <div id="fb-root"></div>
+    <script>
+      window.fbAsyncInit = function() {
+        FB.init({
+          xfbml            : true,
+          version          : 'v3.2'
+        });
+      };
+
+      (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = 'https://connect.facebook.net/es_LA/sdk/xfbml.customerchat.js';
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+
+    <!-- Your customer chat code -->
+    <div class="fb-customerchat"
+      attribution=setup_tool
+      page_id="388026761396087"
+      logged_in_greeting="Hola! Aquí puedes chatear con nosotros y hacernos consultas. Estamos atentos! :)"
+      logged_out_greeting="Hola! Aquí puedes chatear con nosotros y hacernos consultas. Estamos atentos! :)">
+    </div>
+
     <div class="box-loading loader animate fadeIn">
       {{-- <img src="../images/puff.svg"> --}}
       <img src="../images/logo_loading.svg">
